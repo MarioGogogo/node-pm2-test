77fe39a6f8f1abe8bf0805424917012281ec8e55
Mario <148373644@qq.com>
2019-05-28 15:59:42 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const http = require("http");
 
 const hostname = "localhost";
-const port = 1234;
+const port = "1234";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.write(`<h1>Hello world</h1>`);
+  res.write(`<h1>Hello world123</h1>`);
   res.end();
 });
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
